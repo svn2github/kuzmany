a6cc65f8147bd18efd41edc8c3614460a1fd0b2f
xxl <xxl@b0cde658-0ba2-406e-9e10-b418bbee15bc>
2011-01-22 14:38:56 +0000

    Add assign to translator
    git-svn-id: http://svn.cmsmadesimple.org/svn/mlecms@14 b0cde658-0ba2-406e-9e10-b418bbee15bc

--- trunk/action.translator.php
+++ trunk/action.translator.php
@@ -3,15 +3,17 @@
 if (!isset($gCms))
     exit;
 
-$smarty =& cmsms()->GetSmarty();
-$vars['editKey']   = $params['text'];
+$smarty = & cmsms()->GetSmarty();
+$vars['editKey'] = $params['text'];
 // set key
 Translation::getValue($vars);
 // set lang key
-if($smarty->get_template_vars('lang_locale')) Translation::$defFile =  $smarty->get_template_vars('lang_locale');
-echo Translation::getValue($vars);
+if ($smarty->get_template_vars('lang_locale'))
+    Translation::$defFile = $smarty->get_template_vars('lang_locale');
 
-
-
+if(isSet($params["assign"]))
+$smarty->assign($params["assign"], Translation::getValue($vars));
+else 
+    echo Translation::getValue($vars);
 
 ?>
